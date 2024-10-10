d3817394544933f324478ebf03519787a24f0583
Arthur Pradier <arthur.pradier81@gmail.com>
2024-10-10 18:52:44 +0200

    feature(TP1):  Fixed Dijkstra, visualization
    Dijkstra wasn't working because i stopped too soon and didn't marked as visited. Visualization with barebones colors on the terrain.

--- Assets/Scripts/TextureGenerator.cs
+++ Assets/Scripts/TextureGenerator.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TextureGenerator
 {
-    public static Texture2D GenerateTexture(float[] hm, int nx, int ny, List<int> playerClicks, List<List<int>> paths)
+    public static Texture2D GenerateTexture(ref float[] hm, ref float[] slope, float maxSlope, int nx, int ny, List<int> playerClicks, List<List<int>> paths)
     {
         var texture = new Texture2D(nx, ny);
         Color[] pixels = new Color[nx * ny];
@@ -12,7 +13,24 @@ public class TextureGenerator
         for(int j = 0; j < ny; j++)
             for (int i = 0; i < nx; i++)
             {
-                pixels[j * nx + i] = Color.white;
+                int index = j * nx + i;
+                if (hm[index] < -0.5f)
+                    pixels[index] = Color.blue;
+                else if (hm[index] < -0.45f)
+                    pixels[index] = Color.yellow;
+                else if (hm[index] < 0.5f)
+                {
+                    float slopeLocal = slope[index] / maxSlope;
+                    pixels[index] = Color.Lerp(Color.green, new Color(.4f, .254f, 0.023f), slope[index] / maxSlope);
+                }
+                else
+                {
+                    if (slope[index] / maxSlope > .5f)
+                        pixels[index] = Color.gray;
+                    else
+                        pixels[index] = Color.white;
+                }
+                    
             }
 
         // color the pixels around the clicked pixel in red
